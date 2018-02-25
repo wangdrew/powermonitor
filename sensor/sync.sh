a8860ecfe48b822c96ef0e3499f178f426db7b42
Andrew Wang <wang.andrew11@gmail.com>
2018-02-24 16:33:03 -0800

    renaming and python virtualenv

---
+++ sensor/sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp PowerMetric.py pi@192.168.69.192:/home/pi/dev/powermonitor/src
+scp DataStore.py pi@192.168.69.192:/home/pi/dev/powermonitor/src
+scp ReadSensor.py pi@192.168.69.192:/home/pi/dev/powermonitor/src
